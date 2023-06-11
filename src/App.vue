1c4b310ea6ba317cf8b84abb8731d7b222f41fde
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2023-06-11 19:35:20 +0300

    init: start task

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <appHeader />
+  </div>
+</template>
+
+<script>
+import appHeader from './components/appHeader'
+
+export default {
+  name: 'App',
+  components: {
+    appHeader
+  },
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+@import './assets/styles/styles.scss';
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  color: var(mainColor);
+  font-size: 2rem;
+
+}
+</style>
---
+++ src/components/appHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/styles/styles.scss';
+
+div {
+  display: inline-block;
+}
+</style>
